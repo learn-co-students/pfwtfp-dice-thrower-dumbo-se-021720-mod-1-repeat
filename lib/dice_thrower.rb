2fa0df1c4993733aecde77415a064625555c3737
github username <github email address>
2020-02-19 19:02:30 -0500

    Done.

--- lib/dice_thrower.rb
+++ lib/dice_thrower.rb
@@ -1,1 +1,22 @@
 # Write your solution here
+
+
+def throw_die
+    rand(1..6)
+end 
+
+def generate_set(number) 
+    
+    array = [] 
+  
+ number.times do  
+
+    array << throw_die 
+end
+
+array 
+
+end  
+
+print throw_die 
+print generate_set(2)
